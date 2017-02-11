304ae3fa02c1c739860c9a3596b8b8aa74d5dd00
m8ker <haight@gmail.com>
2017-02-11 07:45:04 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,19 +7,7 @@ function getRandomArbitrary (min, max) {
 $(".more_btn").click(function() { 
 	$(".more").toggleClass( "show" );
 });
-function getDocHeight() {
-    var D = document;
-    return Math.max(
-        D.body.scrollHeight, D.documentElement.scrollHeight,
-        D.body.offsetHeight, D.documentElement.offsetHeight,
-        D.body.clientHeight, D.documentElement.clientHeight
-    );
-}
-$(window).scroll(function() {
-       if($(window).scrollTop() + $(window).height() == getDocHeight()) {
-           alert("bottom!");
-       }
-   });
+
 //This ajax request calls 10 initial sets of random array elements strung together (on page load). 
 $.ajax({
   type: "GET",
@@ -46,6 +34,34 @@ are hidden from fiew above the scrollable area. When $(document).height() - $(wi
 scrollbar position, this means we have scrolled to the bottom of the document.  This is our signal that ajax 
 needs to add additional content to the page. 
 */
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+}
+$(window).scroll(function() {
+       if($(window).scrollTop() + $(window).height() == getDocHeight()) {
+           $.ajax({
+		type: "GET",
+			  url: "http://m8ker.github.io/machinestories/js/data.js",
+			  dataType: "script",
+		success: function() {
+			for (var i = 1; i <= 10; i++) {
+				var random_occ = getRandomArbitrary (0, occupations.length),
+								random_adj = getRandomArbitrary (0, adjectives.length),
+		      random_vrb = getRandomArbitrary (0, verbs.length),
+		      random_adv = getRandomArbitrary (0, adverbs.length),
+								random_emo = getRandomArbitrary (0, emotions.length);
+		$("#postswrapper").append('<p><span class="adj">'+adjectives[random_adj]+"</span>"+" "+occupations[random_occ]+" "+"feeling"+" "+'<span class="emo">'+emotions[random_emo]+"</span>"+","+" "+verbs[random_vrb]+"s"+" "+adverbs[random_adv]+"."+"</p>");
+		  };
+		}
+	    });
+       }
+   });
+
 /*$(window).scroll(function() 
 {
   if($(window).scrollTop() == $(document).height() - $(window).height())
